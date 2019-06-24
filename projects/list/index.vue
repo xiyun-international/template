f0fd3faa5c81d50ea12ca8f7d3392aa46d20980c
huqiangqiang <huqiangqiang@xiyun.com.cn>
2019-06-24 18:56:41 +0800

    fix: 列表模板修改

--- projects/list/index.vue
+++ projects/list/index.vue
@@ -1,61 +1,38 @@
 <template>
     <div>
-        <xy-title>XXX列表</xy-title>
-        <xy-wrapper> <search-form @onSearch="onSearch"></search-form> </xy-wrapper>
+        <xy-title>列表</xy-title>
+        <xy-wrapper>
+            <search-form @onSearch="onSearch"></search-form>
+        </xy-wrapper>
         <div class="mt-24">
-            <el-button type="primary" @click="goCreate">创建</el-button>
+            <router-link to="/create">
+                <el-button type="primary">创建</el-button>
+            </router-link>
             <el-button type="default">导出 Excal</el-button>
         </div>
         <xy-wrapper class="mt-24">
             <el-table ref="table" :data="tableData" style="width: 100%">
-                <el-table-column label="编号">
-                    <template slot-scope="scope">
-                        <div> {{ scope.row.code }} </div>
-                    </template>
-                </el-table-column>
                 <el-table-column label="姓名">
-                    <template slot-scope="scope">
-                        <div> {{ scope.row.name }} </div>
-                    </template>
-                </el-table-column>
-                <el-table-column label="分类">
-                    <template slot-scope="scope">
-                        <div> {{ scope.row.type }} </div>
+                    <template>
+                        禧云001
                     </template>
                 </el-table-column>
                 <el-table-column label="状态">
-                    <template slot-scope="scope">
-                        <div> {{ scope.row.status }} </div>
-                    </template>
-                </el-table-column>
-                <el-table-column label="手机号码">
-                    <template slot-scope="scope">
-                        <div> {{ scope.row.tel }} </div>
+                    <template>
+                        状态1
                     </template>
                 </el-table-column>
                 <el-table-column label="操作" align="center" min-width="80">
                     <template slot-scope="scope">
-                        <el-button
-                            @click="goDetail(scope.row.userId)"
-                            type="text"
-                            size="small"
-                        >
-                            查看
-                        </el-button>
-                        <el-button 
-                            @click="goEdit(scope.row.userId)" 
-                            type="text" 
-                            size="small"
-                        >
-                            编辑
-                        </el-button>
-                        <el-button 
-                            type="text" 
-                            size="small" 
-                            @click="goChange(scope.row.userId)"
-                        >
-                            变更
-                        </el-button>
+                        <router-link :to="`/detail/${scope.row.userId}`">
+                            <el-button size="small" type="text">查看</el-button>
+                        </router-link>
+                        <router-link :to="`/edit/${scope.row.userId}`">
+                            <el-button size="small" type="text">编辑</el-button>
+                        </router-link>
+                        <router-link :to="`/change/${scope.row.userId}`">
+                            <el-button size="small" type="text">变更</el-button>
+                        </router-link>
                     </template>
                 </el-table-column>
             </el-table>
@@ -86,24 +63,7 @@ export default {
             searchForm: {},
 
             // 表格数据
-            tableData: [
-                {
-                   userId: 1,
-                   code: '1111111',
-                   name: '禧云',
-                   type: '测试001',
-                   status: '状态1',
-                   tel: '18588888888', 
-                },
-                {
-                   userId: 2,
-                   code: '222222',
-                   name: '禧云',
-                   type: '测试002',
-                   status: '状态2',
-                   tel: '18566666666', 
-                },
-            ],
+            tableData: [],
 
             // 页脚
             page: {
@@ -111,9 +71,6 @@ export default {
                 total: 0,
                 pageSize: 10,
             },
-
-            // 获取路由信息
-            path: this.$route.path,
         }
     },
     components: {
@@ -142,34 +99,6 @@ export default {
                 currentPage: this.page.currentPage,
                 pageSize: this.page.pageSize,
             })
-        },
-
-        /**
-         *  点击跳转到创建页面
-         */
-        goCreate() {
-            this.$router.push(`${this.path}/create`);
-        },
-
-        /**
-         *  点击跳转详情页面
-         */
-        goDetail(id) {
-            this.$router.push(`${this.path}/detail/${id}`);
-        },
-
-        /**
-         *  点击跳转编辑页面
-         */
-        goEdit(id) {
-            this.$router.push(`${this.path}/edit/${id}`);
-        },
-
-        /**
-         *  点击跳转变更页面
-         */
-        goChange(id) {
-            this.$router.push(`${this.path}/change/${id}`);
         },
 
         /**
